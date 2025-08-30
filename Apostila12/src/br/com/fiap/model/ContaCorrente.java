0cd3b9ea8cc753a1ae419ff14b7cbd9526fa5f91
unknown <profthiagoy@fiap.com.br>
2025-08-29 22:44:04 -0300

    Exceptions

--- Apostila12/src/br/com/fiap/model/ContaCorrente.java
+++ Apostila12/src/br/com/fiap/model/ContaCorrente.java
@@ -5,12 +5,21 @@ public class ContaCorrente {
     private double saldo;
 
     //Criar os métodos Sacar, Depositar e Consultar
-    public void sacar(double valor){
+    public void sacar(double valor) throws Exception {
+        validarValor(valor);
+        if (valor > saldo)
+            throw new Exception("Saldo insuficiente, saldo atual " + saldo);
         saldo -= valor;
     }
 
-    public void depositar(double valor){
+    public void depositar(double valor) throws Exception {
+        validarValor(valor);
         saldo += valor;
+    }
+
+    private void validarValor(double valor) throws Exception {
+        if (valor < 0)
+            throw new Exception("Valor não pode ser negativo");
     }
 
     public double consultarSaldo(){
--- Apostila12/src/br/com/fiap/view/Exemplo02.java
+++ Apostila12/src/br/com/fiap/view/Exemplo02.java
@@ -1,20 +1,49 @@
 package br.com.fiap.view;
 
+import br.com.fiap.model.ContaCorrente;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exemplo02 {
 
     public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente();
         Scanner leitor = new Scanner(System.in);
         //Menu: 1-Depositar 2-Sacar 3-Exibir Saldo 0-Sair
-        int opcao;
+        int opcao = 1;
         do {
+
             System.out.println("Escolha: \n1-Depositar \n2-Sacar \n3-Saldo \n0-Sair");
-            opcao = leitor.nextInt();
+            try {
+                opcao = leitor.nextInt();
 
-            
-
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Informe o valor para depósito");
+                        double valor = leitor.nextDouble();
+                        cc.depositar(valor);
+                        break;
+                    case 2:
+                        System.out.println("Informe o valor para saque");
+                        valor = leitor.nextDouble();
+                        cc.sacar(valor);
+                        break;
+                    case 3:
+                        System.out.println("Saldo atual: " + cc.consultarSaldo());
+                        break;
+                    case 0:
+                        System.out.println("Finalizando o programa...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida");
+                }
+            }  catch (InputMismatchException e){
+                leitor.next();
+                System.err.println("Valor inválido");
+            }  catch (Exception e){
+                System.err.println(e.getMessage());
+            }
         } while(opcao != 0);
-
     }//main
 }//class
