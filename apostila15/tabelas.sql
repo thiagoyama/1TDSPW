a98d774dd95037d10ffb106d543bf5f7583dc1ea
unknown <profthiagoy@fiap.com.br>
2025-10-24 22:54:24 -0300

    API com relacionamentos

--- apostila15/tabelas.sql
+++ apostila15/tabelas.sql
@@ -13,4 +13,8 @@ create table t_tdspw_avaliacao (
     ds_avaliacao varchar2(200),
     dt_cadastro date,
     vl_nota number(4,2),
+    cd_doce number(6,0) not null,
+    constraint fk_cd_doce foreign key (cd_doce) references t_tdspw_doce (cd_doce)
 );
+
+create sequence sq_tdspw_avaliacao start with 1 increment by 1 nocache;
