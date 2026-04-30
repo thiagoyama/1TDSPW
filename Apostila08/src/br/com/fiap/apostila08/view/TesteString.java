fa29600b47589bfb7620f81614b7ca68df2dc91a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-29 22:44:53 -0300

    Strings

---
+++ Apostila08/src/br/com/fiap/apostila08/view/TesteString.java
@@ -0,0 +1,27 @@
+package br.com.fiap.apostila08.view;
+
+import javax.swing.*;
+
+public class TesteString {
+
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Digite o nome");
+        System.out.println(nome + " é aluno da \"FIAP\".\nVamos no Evento!");
+
+        //Verificar se o nome é "Jose" "jose" "jOSe"
+        if (nome.equalsIgnoreCase("Jose")){
+            System.out.println("Nome é Jose");
+        } else {
+            System.out.println("Não é o Jose");
+        }
+
+        //Exibir a quantidade de caracteres do nome
+        System.out.println("Qtd de caracteres: " + nome.length());
+
+        //Imprimir o nome na vertical
+        for (int i = 0; i < nome.length(); i++){
+            System.out.println(nome.charAt(i));
+        }
+
+    }//main
+}//class
---
+++ Apostila08/src/br/com/fiap/apostila08/view/TesteString2.java
@@ -0,0 +1,12 @@
+package br.com.fiap.apostila08.view;
+
+import javax.swing.*;
+
+public class TesteString2 {
+
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Digite o email");
+        //Validar se é um e-mail válido (possui @ e .)
+        
+    }
+}
